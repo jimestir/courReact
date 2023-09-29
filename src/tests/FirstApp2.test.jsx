313169aa48ce733c407d0ce7815ed53512dc5791
jimestir <jimmyestir@gmail.com>
2023-09-29 16:43:24 +0200

    add optimized FirstApp2.test.jsx file

---
+++ src/tests/FirstApp2.test.jsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react'
+import FirstApp from '../FirstApp'
+
+describe('test in <FirstApp />', () => {
+  const title = 'Hello i am Jimmy'
+  const numberPage = 24
+  const subTitle = 'soy un subtitulo'
+
+  test('Debe hacer match con el snapshot', () => {
+    const { container } = render(<FirstApp title={title} numberPage={numberPage} />)
+    expect(container).toMatchSnapshot()
+  })
+  test('Debe de mostrar el mensaje "Hello i am Jimmy"', () => {
+    render(<FirstApp title={title} numberPage={numberPage} />)
+    screen.debug()
+    expect(screen.getByText(title)).toBeTruthy()
+  })
+  test('Debe de mostrar el titulo en un h1', () => {
+    render(<FirstApp title={title} numberPage={numberPage} />)
+    expect(screen.getByRole('heading', { level: 1 }).innerHTML).toContain(title)
+  })
+  test('Debe de mostar el subtitulo enviado por props', () => {
+    render(<FirstApp title={title} Subtitle={subTitle} numberPage={numberPage} />)
+    expect(screen.getAllByText(subTitle).length).toBe(3)
+  })
+})
